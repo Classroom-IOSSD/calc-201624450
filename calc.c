df6218e48d445d8a21a84e5a3039c67cffb633a9
201624450 <ansy6969@gmail.com>
2017-06-15 23:40:52 -0700

    lab 9

--- calc.c
+++ calc.c
@@ -7,28 +7,29 @@ int main(){
 	char operator = ' ';
 	double result;
 	int line = 0;
-
+	int i;
+	double (*op) (double, double);
 	fp = fopen("read.txt","r");
 	if(fp!=NULL){
-		fscanf(fp, "%d", &line);
-	
-		for(int i=0; i<line-1; i++) {
+		fscanf(fp, "%d", &line);	
+		for(i=0; i<line-1; i++) {
 			fscanf(fp, "%lf %c %lf",&operand1, &operator, &operand2);
 			switch(operator) {
 				case '+':
-				result = add(operand1, operand2);
+				op = add;
 				break;
 				case '-':
-				result = minus(operand1, operand2);
+				op = minus;
 				break;
 				case '*':
-				result = mul(operand1, operand2);
+				op = mul;
+				break;
 				case '/':
-				result = div(operand1, operand2);
+				op = div;
 				break;
 			}		
-			printf("%f %c %f = %f\n",
-				 operand1, operator, operand2, result);
+			result = op(operand1, operand2);
+			printf("%f %c %f = %f\n",operand1, operator, operand2, result);
 		}
 	}
 	return 0;
